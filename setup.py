d33c7409c16e0eaa42f02a8473ffc3a81042c328
cortes.cj <cortes.cj@ntsp.nec.co.jp>
2020-10-22 10:33:11 +0800

    UPDATE: updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 try:
     from setuptools import setup
 except ImportError:
@@ -14,8 +17,17 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [str(i.req) for i in parse_requirements('requirements.txt', session=False)]
-test_requirements = [str(i.req) for i in parse_requirements('requirements_dev.txt', session=False)]
+try:
+    requirements = [str(i.req) for i in parse_requirements('requirements.txt', session=False)]
+except:
+    requirements = [str(i.requirement) for i in parse_requirements('requirements.txt', session=False)]
+
+try:
+    test_requirements = [str(i.req) for i in parse_requirements('requirements_dev.txt', session=False)]
+except:
+    test_requirements = [str(i.requirement) for i in parse_requirements('requirements_dev.txt', session=False)]
+   
+
 
 setup(
     name='swagger_parser',
